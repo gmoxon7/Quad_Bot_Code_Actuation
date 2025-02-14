4a6e82cb3b124d6a3b5ef1acf175397a2a1543c2
gmoxon7 <gmoxon7@gmail.com>
2025-02-14 16:01:35 +0000

    include spi and i2c libraries

--- Quadruped_Bot_Actuation_Code/src/main.cpp
+++ Quadruped_Bot_Actuation_Code/src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
 
+
+#include <Wire.h>
+//#include <Wire1.h> // for teensy 4.0 this is the i2c library on pins 17(sda1) and 16(sc1)
+
+#include <SPI.h> // for the rotary encoder
+
 // put function declarations here:
 int myFunction(int, int);
 
